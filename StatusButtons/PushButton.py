5a6a9267a2b88e93be0bdf8722f6444700cfa9b3
defmylife <kantaka.dev@gmail.com>
2023-10-27 10:38:26 +0700

    ✨ Status PushButton

---
+++ StatusButtons/PushButton.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+import typing
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class StatusPushButton(QtWidgets.QWidget):
+    # activated = QtCore.pyqtSignal(str)
+    
+    def __init__(self, 
+        parent: typing.Optional[QtWidgets.QWidget] = ...,
+        init: str = "No Connection"
+    ) -> None: 
+        super(QtWidgets.QWidget, self).__init__(parent)
+
+        self.DOT_STYLE = ("QWidget {\n"
+            "border-radius: 6px;\n"
+            "background-color: {OPTION};\n"
+            "}")
+
+        self.setupUi(init)
+
+    def setupUi(self, initial_text: str = "No Connection"):
+
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
+        self.setSizePolicy(sizePolicy)
+        self.setMinimumSize(QtCore.QSize(0, 30))
+
+        self.setStyleSheet("QPushButton {\n"
+"background-color: #FFF;\n"
+"color: #B5BFC6; \n"
+"border-radius: 14px;\n"
+"text-align: center; \n"
+"\n"
+"selection-background-color: #FFF; /*ECECEC*/\n"
+"selection-color: #0F5BCD;\n"
+"outline: 0;\n"
+"padding-left: 12px;\n"
+"padding-right: 12px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"    background-color: #F8F9FB; \n"
+"}\n"
+# "QComboBox:on { /* shift the text when the popup opens */\n"
+# "    color: #B5BFC6\n"
+# "}\n"
+# "QComboBox::drop-down {\n"
+# "border: 0px;\n"
+# "padding-right: 5px;\n"
+# "}\n"
+# "QComboBox::down-arrow {\n"
+# "/* image: url(:/icons/icons/edit.png); width: 16px; height: 16px; */\n"
+# "}\n"
+# "\n"
+# "QComboBox QListView {\n"
+# "background-color: #F8F9FB;\n"
+# "color: #B5BFC6; \n"
+# "border-radius: 8px;\n"
+# "padding: 5px; margin-top: 5px;\n"
+# "outline: 1px;\n"
+# "}\n"
+# "QComboBox QAbstractItemView {\n"
+# "    selection-background-color: #EDF0F6;\n"
+# "    selection-color: #4487EB;\n"
+# "}\n"
+"\n")
+        self.setObjectName("statusButtonFrame")
+
+        self.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
+
+        #########################
+        #         Layout        #
+        #########################
+        self.__layout = QtWidgets.QHBoxLayout(self)
+        self.__layout.setContentsMargins(12, 0, 0, 0)
+        self.__layout.setSpacing(4)
+        self.__layout.setObjectName("layout")
+
+        #########################
+        #       Status Dot      #
+        #########################
+        self.__dot = QtWidgets.QWidget(self)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.__dot.sizePolicy().hasHeightForWidth())
+        self.__dot.setSizePolicy(sizePolicy)
+        self.__dot.setMinimumSize(QtCore.QSize(12, 12))
+        self.__dot.setMaximumSize(QtCore.QSize(12, 12))
+        self.__dot.setObjectName("dot")
+        self.__layout.addWidget(self.__dot)
+        self.setStatusIdle()
+    
+        #########################
+        #       PushButton      #
+        #########################
+        self.statusButton = QtWidgets.QPushButton(self)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.statusButton.sizePolicy().hasHeightForWidth())
+        self.statusButton.setSizePolicy(sizePolicy)
+        self.statusButton.setMinimumSize(QtCore.QSize(0, 30))
+
+        font = QtGui.QFont()
+        font.setFamily("Montserrat SemiBold")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.statusButton.setFont(font)
+        self.statusButton.setText(initial_text)
+
+        # self.statusButton.setSizeAdjustPolicy(QtWidgets.QComboBox.AdjustToContents)
+        self.statusButton.setObjectName("statusButton")
+        self.__layout.addWidget(self.statusButton)
+    
+    def setStatusIdle(self, text :str = None) -> None: 
+        self.__dot.setStyleSheet( 
+            self.DOT_STYLE.replace('{OPTION}', '#B5BFC6') 
+        )
+        if text is not None: self.statusButton.setText(text)
+    def setStatusConnected(self, text :str = None) -> None: 
+        self.__dot.setStyleSheet( 
+            self.DOT_STYLE.replace('{OPTION}', '#04B56E') 
+        )
+        if text is not None: self.statusButton.setText(text)
+    def setStatusConnecting(self, text :str = None) -> None: 
+        self.__dot.setStyleSheet( 
+            self.DOT_STYLE.replace('{OPTION}', '#F99500') 
+        )
+        if text is not None: self.statusButton.setText(text)
+    def setStatusDisconnected(self, text :str = None) -> None: 
+        self.__dot.setStyleSheet( 
+            self.DOT_STYLE.replace('{OPTION}', '#F25858') 
+        )
+        if text is not None: self.statusButton.setText(text)
+
+
+    # def addItem(self, text: str, userData: typing.Any = ...) -> None: 
+    #     self.comboBox.addItem(text, userData)
+    # def addItems(self, texts: typing.Iterable[str]) -> None:
+    #     self.comboBox.addItems(texts)
+    # def clear(self) -> None: 
+    #     self.comboBox.clear()
+
+
+if __name__ == '__main__':
+
+    app = QtWidgets.QApplication([])
+    
+    window = QtWidgets.QWidget()
+    window.setStyleSheet("QWidget {background-color: #FFF;}")
+
+    layout = QtWidgets.QHBoxLayout(window)
+    layout.setContentsMargins(12, 12, 12, 12)
+    layout.setSpacing(12)
+    layout.setObjectName("layout")
+
+    #TUTORIAL ######################################
+    statusBox1 = StatusPushButton(window)
+    
+    statusBox1.statusButton.clicked.connect(print) # Print the index after comboBox get clicked
+
+    statusBox2 = StatusPushButton(window)
+    statusBox2.setStatusConnecting(text="Connecting")
+
+    statusBox3 = StatusPushButton(window)
+    statusBox3.setStatusConnected(text="Connected")
+
+    statusBox4 = StatusPushButton(window)
+    statusBox4.setStatusDisconnected(text="Error")
+
+    layout.addWidget(statusBox1)
+    layout.addWidget(statusBox2)
+    layout.addWidget(statusBox3)
+    layout.addWidget(statusBox4)
+    window.update()
+    ################################################
+
+    spacer = QtWidgets.QSpacerItem(261, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+    layout.addItem(spacer)
+    window.show()
+    app.exec_()
